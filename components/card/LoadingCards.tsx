e4e14994a93f1ff688bacbbf3e7a8654952dcee0
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-24 06:15:31 +0300

    loading skeleton

--- components/card/LoadingCards.tsx
+++ components/card/LoadingCards.tsx
@@ -1,6 +1,21 @@
+import { Skeleton } from '@/components/ui/skeleton';
 const LoadingCards = () => {
   return (
-    <div>LoadingCards</div>
-  )
+    <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+    </section>
+  );
+};
+export function SkeletonCard() {
+  return (
+    <div>
+      <Skeleton className="h-[300px] rounded-md" />
+      <Skeleton className="h-4 mt-2 w-3/4" />
+      <Skeleton className="h-4 mt-2 w-1/2" />
+    </div>
+  );
 }
-export default LoadingCards
+export default LoadingCards;
