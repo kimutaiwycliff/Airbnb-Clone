b430936dbea4646d97cdb15085c6e39c102a1a1d
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-24 05:19:04 +0300

    text area input for property

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -4,6 +4,7 @@ import { createPropertyAction } from '@/utils/actions';
 import { SubmitButton } from '@/components/form/Buttons';
 import PriceInput from '@/components/form/PriceInput';
 import CategoriesInput from '@/components/form/CategoriesInput';
+import TextAreaInput from '@/components/form/TextAreaInput';
 const CreatePropertyPage = () => {
   return (
     <section>
@@ -29,7 +30,7 @@ const CreatePropertyPage = () => {
             <PriceInput />
             <CategoriesInput />
           </div>
-          {/* text area / description */}
+          <TextAreaInput name='description' labelText='Description (10 - 1000 Words)' />
           <SubmitButton text='create rental' className='mt-12' />
         </FormContainer>
       </div>
