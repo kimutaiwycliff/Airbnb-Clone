6f053b53929450acbab36e37a925e3d54ba59b8e
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-24 06:05:45 +0300

    property rating component

--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,6 +1,22 @@
-const PropertyRating = () => {
+import { FaStar } from 'react-icons/fa';
+const PropertyRating = async ({
+  propertyId,
+  inPage,
+}: {
+  propertyId: string;
+  inPage: boolean;
+}) => {
+  const rating = 4.7;
+  const count = 100;
+
+  const className = `flex gap-1 items-center ${inPage ? 'text-md' : 'text-xs'}`;
+  const countText = count > 1 ? 'reviews' : 'review';
+  const countValue = `(${count}) ${inPage ? countText : ''}`;
   return (
-    <div>PropertyRating</div>
-  )
+    <span className={className}>
+      <FaStar className='w-3 h-3' />
+      {rating} {countValue}
+    </span>
+  );
 }
 export default PropertyRating
