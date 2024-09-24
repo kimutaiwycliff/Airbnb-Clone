5b00a6433ecd9cfe1d9b52b86a25c922855f7239
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-24 06:12:29 +0300

    country flag and name

--- components/card/CountryFlagAndName.tsx
+++ components/card/CountryFlagAndName.tsx
@@ -1,6 +1,14 @@
-const CountryFlagAndName = () => {
+import { findCountryByCode } from '@/utils/countries';
+const CountryFlagAndName = ({ countryCode }: { countryCode: string }) => {
+  const validCountry = findCountryByCode(countryCode);
+  const countryName =
+    validCountry!.name.length > 20
+      ? `${validCountry!.name.substring(0, 20)}...`
+      : validCountry!.name;
   return (
-    <div>CountryFlagAndName</div>
-  )
+    <span className='flex justify-between items-center gap-2 text-sm '>
+      {validCountry?.flag} {countryName}
+    </span>
+  );
 }
 export default CountryFlagAndName
